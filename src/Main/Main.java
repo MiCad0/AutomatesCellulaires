28d5f46de9c860f7f066b003ac18d0e427e4c654
MiCad0 <denislinde4@gmail.com>
2024-06-06 16:45:36 +0200

    almost finalized

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,116 +1,130 @@
 package Main;
 
-import Operateurs.COMPTER;
-import Operateurs.CONST;
-import Operateurs.EQ;
-import Operateurs.OU;
-import Operateurs.Operateur;
-import Operateurs.SI;
-import Types.Coords;
-import Types.G8;
-import Types.G8e;
-import Types.TableauDynamiqueND;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Graphics;
+import Types.*;
+import Operateurs.*;
 
 
-public class Main 
-{
-	public static void main(String[] args)
-	{
 
-		TableauDynamiqueND tab = new TableauDynamiqueND(new Coords(true, 2), 30, 30);
+public class Main extends JFrame {
+	private static final int WIDTH = 816;
+	private static final int HEIGHT = 866;
+	private static Boolean running = true;
 
-		Coords[] alive = tab.getAlive();
+	private TableauDynamiqueND tab;
 
-		for(Coords a : alive)
-		{
-			if(a != null)
-			{
-				a.display();
-			}
-		}
+	public Main() {
+		tab = new TableauDynamiqueND(new Coords(true, 2), 100, 100);
 
-
-		GrilleGraphique grid = new GrilleGraphique(tab.getTaille(), tab.getTab()[0].getTaille(), 8);
-
-		
-		int i;
-		tab.changeState(1,1);
-		grid.colorierCase(1,1);
-		tab.changeState(2,1);
-		grid.colorierCase(1,2);
-		tab.changeState(3,1);
-		grid.colorierCase(1,3);
-
-		// changement de cases random
-		// for(i = 0; i < 100; i++){
-		// 	int x = (int)(Math.random()*50);
-		// 	int y = (int)(Math.random()*50);
-		// 	tab.changeState(x, y);
-		// 	if(tab.getState(x,y))
-		// 	{
-		// 		grid.colorierCase(y,x);
-		// 	}
-		// 	else
-		// 	{
-		// 		grid.effacerCase(y,x);
-		// 	}
-		// }
+		setTitle("Game of Life");
+		setSize(WIDTH, HEIGHT);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 
 		// Vaisseau glider
 		tab.changeState(10,10);
-		grid.colorierCase(10,10);
 		tab.changeState(11,11);
-		grid.colorierCase(11,11);
 		tab.changeState(11,12);
-		grid.colorierCase(12,11);
 		tab.changeState(10,12);
-		grid.colorierCase(12,10);
 		tab.changeState(9,12);
-		grid.colorierCase(12,9);
 
-
-
-		TableauDynamiqueND tmp = tab.clone();
-
-		grid.repaint();
-
-		try{
-			Thread.sleep(100);
-		}
-		catch(InterruptedException e){
-			e.printStackTrace();
-		}
-
-		while(true){
-			for(i = 0; i < 30; i++){
-				for(int j = 0; j < 30; j++){
-					Operateur regle = new OU(new SI(new EQ(new COMPTER(new G8e(tab, i,j)),  new CONST(3)),  new CONST(1),  new CONST(0)), new SI(new EQ(new COMPTER(new G8(tab, i,j)),  new CONST(3)), new CONST(1), new CONST(0)));
-					tmp.setState(regle.evaluer(), i,j);
-				}
-			}
-			for(i = 0; i < 30; i++){
-				for(int j = 0; j < 30; j++){
-					if(tmp.getState(i,j)){
-						grid.colorierCase(j,i);
-					}
-					else{
-						grid.effacerCase(j,i);
-					}
-					try{
-						Thread.sleep(1);
-					}
-					catch(InterruptedException e){
-						e.printStackTrace();
+		JPanel panel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				for (int i = 0; i < 100; i++) {
+					for (int j = 0; j < 100; j++) {
+						if (tab.getState(i, j)) {
+							g.setColor(new Color(255,0,255));
+							g.fillRect(j * 8, i * 8, 8, 8);
+						} else {
+							g.setColor(Color.WHITE);
+							g.fillRect(j * 8, i * 8, 8, 8);
+						}
+						g.setColor(Color.BLACK);
+						g.drawRect(j * 8, i * 8, 8, 8);
 					}
 				}
 			}
+		};
+
+		JButton button = new JButton("Pause");
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (button.getText().equals("Pause")) {
+					button.setText("Reprendre");
+					running = false;
+				} else {
+					button.setText("Pause");
+					running = true;
+				}
+			}
+		});
+
+
+		
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int cellSize = Math.min(panel.getWidth() / 100, panel.getHeight() / 100);
+                int row = e.getY() / cellSize;
+                int col = e.getX() / cellSize;
+
+                tab.changeState(row, col);
+                panel.repaint();
+            }
+        });
+
+
+		add(panel);
+		add(button, "South");
+
+		setVisible(true);
+
+		runGameOfLife();
+	}
+
+	private void runGameOfLife() {
+		while (true) {
+			if (!running) {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				continue;
+			}
+			TableauDynamiqueND tmp = tab.clone();
+
+			for (int i = 0; i < 100; i++) {
+				for (int j = 0; j < 100; j++) {
+					Operateur regle = new OU(new SI(new EQ(new COMPTER(new G8e(tab, i, j)), new CONST(3)), new CONST(1), new CONST(0)), new SI(new EQ(new COMPTER(new G8(tab, i, j)), new CONST(3)), new CONST(1), new CONST(0)));
+					tmp.setState(regle.evaluer(), i, j);
+				}
+			}
+
 			tab = tmp.clone();
-			try{
+
+			repaint();
+
+			try {
 				Thread.sleep(30);
-			}
-			catch(InterruptedException e){
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public static void main(String[] args) {
+		new Main();
 	}
 }

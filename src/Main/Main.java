06171dfb9a3557ee8b1bbb6c5186c720bd7b6bf4
MiCad0 <denislinde4@gmail.com>
2024-06-06 17:11:37 +0200

    JUST ADD PARSING FFS

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,16 +15,17 @@ import Operateurs.*;
 
 
 public class Main extends JFrame {
-	private static final int WIDTH = 816;
-	private static final int HEIGHT = 866;
+	private static int WIDTH;
+	private static int HEIGHT;
 	private static Boolean running = true;
 
 	private TableauDynamiqueND tab;
 
 	public Main() {
 		tab = new TableauDynamiqueND(new Coords(true, 2), 100, 100);
-
-		setTitle("Game of Life");
+		WIDTH = 8*tab.getTaille()+16;
+		HEIGHT = 8*tab.getTab()[0].getTaille()+66;
+		setTitle("Jeu de la Vie!");
 		setSize(WIDTH, HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
@@ -36,12 +37,14 @@ public class Main extends JFrame {
 		tab.changeState(10,12);
 		tab.changeState(9,12);
 
+
+		// Interface Graphique
 		JPanel panel = new JPanel() {
 			@Override
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				for (int i = 0; i < 100; i++) {
-					for (int j = 0; j < 100; j++) {
+				for (int i = 0; i < tab.getTaille(); i++) {
+					for (int j = 0; j < tab.getTab()[1].getTaille(); j++) {
 						if (tab.getState(i, j)) {
 							g.setColor(new Color(255,0,255));
 							g.fillRect(j * 8, i * 8, 8, 8);
@@ -56,6 +59,8 @@ public class Main extends JFrame {
 			}
 		};
 
+
+		// Bouton pause
 		JButton button = new JButton("Pause");
 		button.addActionListener(new ActionListener() {
 			@Override
@@ -71,16 +76,16 @@ public class Main extends JFrame {
 		});
 
 
-		
-        panel.addMouseListener(new MouseAdapter() {
+		// Gestion de la souris
+        panel.addMouseListener(new MouseAdapter() {				
             @Override
             public void mouseClicked(MouseEvent e) {
-                int cellSize = Math.min(panel.getWidth() / 100, panel.getHeight() / 100);
+                int cellSize = Math.min(panel.getWidth() / tab.getTaille(), panel.getHeight() / tab.getTab()[1].getTaille());
                 int row = e.getY() / cellSize;
                 int col = e.getX() / cellSize;
 
-                tab.changeState(row, col);
-                panel.repaint();
+				tab.changeState(row, col);
+                repaint();
             }
         });
 
@@ -105,8 +110,8 @@ public class Main extends JFrame {
 			}
 			TableauDynamiqueND tmp = tab.clone();
 
-			for (int i = 0; i < 100; i++) {
-				for (int j = 0; j < 100; j++) {
+			for (int i = 0; i < tab.getTaille(); i++) {
+				for (int j = 0; j < tab.getTab()[1].getTaille(); j++) {
 					Operateur regle = new OU(new SI(new EQ(new COMPTER(new G8e(tab, i, j)), new CONST(3)), new CONST(1), new CONST(0)), new SI(new EQ(new COMPTER(new G8(tab, i, j)), new CONST(3)), new CONST(1), new CONST(0)));
 					tmp.setState(regle.evaluer(), i, j);
 				}

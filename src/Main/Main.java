627c0f2f372fcff12bfedc8c5b5cd5027ff6bf6a
MiCad0 <denislinde4@gmail.com>
2024-06-10 07:00:07 +0200

    Refactor Main class to use JSlider for selecting depth

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,10 +2,12 @@ package Main;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.management.openmbean.OpenType;
 import javax.swing.JButton;
 import javax.swing.JSlider;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
@@ -20,52 +22,42 @@ public class Main extends JFrame {
 	private static int HEIGHT;
 	private static Boolean running = true;
 	private static int profondeur = 0;
+	TableauDynamiqueND tabToDisplay;
 
-	private TableauDynamiqueND tab;
+	private static TableauDynamiqueND tab;
 
-	public Main() {
-		tab = new TableauDynamiqueND(new Coords(true, 3), 10, 150, 200);
-		WIDTH = 8*tab.getTab()[0].getTab()[0].getTaille()+66;
-		HEIGHT = 8*tab.getTab()[0].getTaille()+66;
+
+	public Main(int [] tailles) {
+		tab = new TableauDynamiqueND(new Coords(true, tailles.length), tailles);
+		tabToDisplay = tab.clone();
+		for(int i = 0; i < tab.getDimension()-2; ++i){
+			tabToDisplay = tabToDisplay.slice(profondeur);
+		}
+
+
+		WIDTH = 8*tabToDisplay.getTab()[0].getTaille()+66;
+		HEIGHT = 8*tabToDisplay.getTaille()+66;
 		setTitle("Jeu de la Vie!");
 		setSize(WIDTH, HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 
-		// Vaisseau glider
-		// tab.changeState(10,10);
-		// tab.changeState(11,11);
-		// tab.changeState(11,12);
-		// tab.changeState(10,12);
-		// tab.changeState(9,12);
 
 		// Blinkers à chaque profondeur
-		for (int i = 0; i < tab.getTaille(); i++) {
+		for (int i = 0; i < tab.getTaille()-2; i++) {
 			tab.changeState(i, 10+i, 10);
 			tab.changeState(i, 11+i, 10);
 			tab.changeState(i, 12+i, 10);
 		}
-
-		// On colorie des cellules aléatoirement
-		for (int i = 0; i < tab.getTaille(); i++) {
-			for (int j = 0; j < tab.getTab()[0].getTaille(); j++) {
-				for (int k = 0; k < tab.getTab()[0].getTab()[0].getTaille(); k++) {
-					if (Math.random() < 0.1) {
-						tab.changeState(i, j, k);
-					}
-				}
-			}
-		}
-
 
 		// Interface Graphique
 		JPanel panel = new JPanel() {
 			@Override
 			protected void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				for (int i = 0; i < tab.getTab()[profondeur].getTaille(); i++) {
-					for (int j = 0; j < tab.getTab()[profondeur].getTab()[0].getTaille(); j++) {
-						if (tab.getState(profondeur, i, j)) {
+				for (int i = 0; i < tabToDisplay.getTaille(); i++) {
+					for (int j = 0; j < tabToDisplay.getTab()[i].getTaille(); j++) {
+						if (tabToDisplay.getState(i, j)) {
 							g.setColor(new Color(255,0,255));
 							g.fillRect(j * 8, i * 8, 8, 8);
 						} else {
@@ -100,39 +92,75 @@ public class Main extends JFrame {
         panel.addMouseListener(new MouseAdapter() {				
             @Override
             public void mouseClicked(MouseEvent e) {
-                int cellSize = Math.min(WIDTH / tab.getTab()[profondeur].getTab()[0].getTaille(), HEIGHT / tab.getTab()[profondeur].getTaille());
+                int cellSize = Math.min(tailles[tailles.length-1]*8 / tabToDisplay.getTab()[0].getTaille(), tailles[tailles.length-2]*8 / tabToDisplay.getTaille());
                 int row = e.getY() / cellSize;
                 int col = e.getX() / cellSize;
-				tab.getTab()[profondeur].changeState(row, col);
-                repaint();
+				int[] tmp = new int[tab.getDimension()];
+				for(int i = 0; i < tab.getDimension()-3; ++i){
+					tmp[i] = 0;
+				}
+				tmp[tab.getDimension()-3] = profondeur;
+				tmp[tab.getDimension()-2] = row;
+				tmp[tab.getDimension()-1] = col;
+				tab.changeState(tmp);
+				tabToDisplay.changeState(row, col);
+				if(!running)
+	                repaint();
             }
         });
 
 
-		// Slider pour la profondeur
+		add(panel);
+		add(button, "South");
+
+
 		JSlider slider = new JSlider(JSlider.VERTICAL, 0, tab.getTaille()-1, profondeur);
-		// slider.setMajorTickSpacing(10);
 		slider.setMajorTickSpacing(1);
 		slider.setPaintTicks(true);
 		slider.setPaintLabels(true);
 		slider.addChangeListener(e -> {
 			profondeur = slider.getValue();
+			tabToDisplay = tab.clone();
+			for(int j = 0; j < tab.getDimension()-2; ++j){
+				tabToDisplay = tabToDisplay.slice(profondeur);
+			}
 		});
 
 
-		add(panel);
-		add(button, "South");
 		add(slider, "East");
 		setVisible(true);
 		runGameOfLife();
 	}
 
-	private void runGameOfLife() {
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+
+	private TableauDynamiqueND update() throws Exception{
+		TableauDynamiqueND tmp = tab.clone();
+		try{
+			if(tab.getDimension() == 2){
+				for (int i = 0; i < tmp.getTaille(); i++) {
+					for (int j = 0; j < tmp.getTab()[i].getTaille(); j++) {
+						Operateur regle = new OU(new SI(new EQ(new COMPTER(new G8e(tab, i, j)), new CONST(3)), new CONST(1), new CONST(0)), new SI(new EQ(new COMPTER(new G8(tab, i, j)), new CONST(3)), new CONST(1), new CONST(0)));
+						int state = regle.evaluer();
+						tmp.setState(state, i, j);
+						tabToDisplay.setState(state, i, j);
+					}
+				}
+			}
+			else{
+				throw new Exception("Dimension non gérée");
+			}
 		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		return tmp;
+	}
+
+
+
+
+
+	private void runGameOfLife() {
 		while (true) {
 			if (!running) {
 				try {
@@ -142,27 +170,23 @@ public class Main extends JFrame {
 				}
 				continue;
 			}
-			TableauDynamiqueND tmp = tab.clone();
-			for (int i = 0; i < tab.getTaille(); i++) {
-				TableauDynamiqueND tabForCalculation = tab.slice(i);
-				for (int j = 0; j < tabForCalculation.getTaille(); j++) {
-					for(int k = 0; k < tabForCalculation.getTab()[j].getTaille(); k++){
-						Operateur regle = new OU(new SI(new EQ(new COMPTER(new G8e(tabForCalculation, j, k)), new CONST(3)), new CONST(1), new CONST(0)), new SI(new EQ(new COMPTER(new G8(tabForCalculation, j, k)), new CONST(3)), new CONST(1), new CONST(0)));
-						tmp.setState(regle.evaluer(), i, j, k);
-					}
-				}
+			try{
+				tab = update();
 			}
-			tab = tmp.clone();
+			catch(Exception e){
+				System.out.println(e);
+			}
 			repaint();
-			try {
-				Thread.sleep(30);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
+
 	public static void main(String[] args) {
-		new Main();
+		// int[] tailles = new int[args.length];
+		// for (int i = 0; i < args.length; i++) {
+		// 	tailles[i] = Integer.parseInt(args[i]);
+		// }
+		int[] tailles = {10, 150, 200};
+		new Main(tailles);
 	}
 }
